36f390f0896bb4a9c3400dc93e41acb1e0722d48
SantyS25 <123515186+SantyS25@users.noreply.github.com>
2024-05-22 20:15:21 -0500

    Add files via upload

--- tarea1.js
+++ tarea1.js
@@ -6,3 +6,9 @@ let dato1 = prompt("Ingresa tu nombre");
 let resultado = "Bienvenido Sr." + " " + dato1;
 
 alert(resultado)
+
+let dato2 = prompt("Ingrese su edad")
+
+let resultado2 = "Muchas Gracias"
+
+alert(resultado2)
